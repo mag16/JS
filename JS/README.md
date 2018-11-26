321cc6e21fa9f8bd9341cb4ac0f97447652e7901
Marco A Guzman <17074832+mag16@users.noreply.github.com>
2018-11-26 14:37:32 -0800

    Add files via upload

---
+++ JS/README.md
@@ -0,0 +1,2 @@
+Javascript Practice with UdemyCourse.
+
---
+++ JS/app.js
@@ -0,0 +1,32 @@
+// //
+
+// console.log('Hello World')
+// console.log(456)
+
+
+// // var, let, const
+
+// var name = 'Guzman'
+// console.log(name)
+
+// const lame = 'Gooze';
+
+// console.log(typeof lame);
+
+// //
+
+// let val;
+
+// val = 5;
+
+// //Output
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length);
+
+val = parseInt('100');
+val = parseFloat('100.30');
+
+
+//output
+console.log(val);
